b3d4fd58523c86d99d56ae5f47a68267219f9772
Nikhil <nik.ch.23@gmail.com>
2019-03-25 20:15:47 +0530

    Libraries imported

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
-import
+import pandas as pd
+import numpy as np
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+url = "https://www.ibbi.gov.in/orders/nclt"
+html = urlopen(url)
+
+print(html)
