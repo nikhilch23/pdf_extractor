1e55b696d93ba5fda40670712192fe334de95d0f
Nikhil <nik.ch.23@gmail.com>
2019-03-27 00:24:06 +0530

    pdf downloader added

--- code.py
+++ code.py
@@ -2,13 +2,26 @@ import pandas as pd
 import numpy as np
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import requests
+import os
 
-num=1
+path = os.getcwd() + "/ADMITTED"
+os.mkdir(path)
+path = os.getcwd() + "/OTHERS"
+os.mkdir(path)
+path = os.getcwd() + "/RESOLUTION"
+os.mkdir(path)
+path = os.getcwd() + "/LIQUIDATION"
+os.mkdir(path)
 
-url = "https://www.ibbi.gov.in/orders/nclt?page=" + str(num)
-html = urlopen(url)
+x = 0
+for num in range(1,158):
 
-soup = BeautifulSoup(html, 'lxml')
+	url = "https://www.ibbi.gov.in/orders/nclt?page=" + str(num)
+	html = urlopen(url)
+
+	soup = BeautifulSoup(html, 'lxml')
 
 # print(soup.prettify())
 
@@ -18,8 +31,39 @@ soup = BeautifulSoup(html, 'lxml')
 #ext = soup.get_text()
 #print(text)
 
-table = soup.find('table', class_="cols-4 responsive-enabled")
-link = table.find_all('a')
+	table = soup.find('table', class_="cols-4 responsive-enabled")
 
+	row = table.find_all('tr')
+
+	orders=""
+	link=""
+	num=1
+	for j in row:
+		c=0
+		column = j.find_all('td')
+		for k in column:
+			c=c+1
+			if c==3:
+				orderstag = str(k)
+				index = re.search("[A-Z]",orderstag).start()
+				orders = orderstag[index:-6].strip()
+			links = j.find('a')
+			string = str(links)
+			index1 = re.search("https", string).start()
+			index2 = re.search(";", string).start()
+			link = str(links)[index1:index2-2]	
+		pdf_url = link	
+		if num!=1:
+			r = requests.get(pdf_url)
+			with open("file" +str(x)+ ".pdf",'wb') as pdf:
+				pdf.write(r.content)
+		num = num+1	
+		x = x+1
+	
+"""
 for i in link:
-	print(i)
+	string = str(i)
+	index1 = re.search("https", string).start()
+	index2 = re.search(";", string).start()
+	print(str(i)[index1:index2-2])	
+"""	
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import urllib.request   #import the library used to query a website
 from bs4 import BeautifulSoup
 import requests 
+import os
+import urllib
 #wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
 
 #page = urllib.request.urlopen(wiki) #Query the website and return the html to the variable 'page'
@@ -9,10 +11,23 @@ import requests
 #print(soup.prettify())
 
 image_url = "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+pdf_url = "https://ibbi.gov.in//webadmin/pdf/order/2019/Mar/12th Mar 2019 in the matter of Vadali Infotech Private Limited C.P. (IB)-4463-MB-2018_2019-03-19 15:01:36.pdf"
 
-r = requests.get(image_url)
+#r = requests.get(pdf_url)
 
-with open("python_logo.png",'wb') as f:
-	f.write(r.content)
+#with open("python_logo.png",'wb') as f:
+#	f.write(r.content)
 
-	
+#r = requests.get(image_url)
+
+#with open("python_logo.pdf",'wb') as pdf:
+#	pdf .write(r.content)
+
+
+path = os.getcwd()
+
+urllib.urlretrieve('https://ibbi.gov.in//webadmin/pdf/order/2019/Mar/12th Mar 2019 in the matter of Vadali Infotech Private Limited C.P. (IB)-4463-MB-2018_2019-03-19 15:01:36.pdf', path)
+
+  
+print ("The current working directory is %s" % path)
+
